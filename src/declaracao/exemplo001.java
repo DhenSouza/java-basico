d91a7f0f7bc13087b5132c493c750960bcec15d1
DhenSouza <ds052798ts@gmail.com>
2021-05-10 16:25:30 -0300

    fazendo alteração no exemplo 1

--- src/declaracao/exemplo001.java
+++ src/declaracao/exemplo001.java
@@ -5,6 +5,10 @@ public class exemplo001 {
     public static void main(String[] args) {
         int[] meuArray = {12,32,54,6,8,89,64};
 
+        meuArray[2] = 10;
+
+        System.out.println(meuArray[2]);
+
         for(int i=0; i<7; i++){
             System.out.println(meuArray[i]);
         }
--- src/declaracao/exemplo002.java
+++ src/declaracao/exemplo002.java
@@ -4,5 +4,14 @@ public class exemplo002 {
 
     public static void main(String[] args) {
 
+        // Arrays multidimencional
+
+        int[][] meuArrayMulti = {{1,2,3,4}, {5,6,7}};
+
+        for (int i = 0; i < meuArrayMulti.length; i++) {
+            for (int j=0; j < meuArrayMulti[i].length; j++) {
+                System.out.println(meuArrayMulti[i][j]);
+            }
+        }
     }
 }
